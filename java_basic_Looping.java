426ec86700082d318a6a31254b05bb681e3f79ce
ardieXjoxong <ardiejocong@gmail.com>
2016-10-30 15:21:16 +0700

    Create java_basic_Looping.java

---
+++ java_basic_Looping.java
@@ -0,0 +1,10 @@
+public class Looping {
+  public static void main(String[]args){
+        int no = 1;
+        while (no < 6) {
+            System.out.print(no + ", ");
+            no += 1;
+        }
+        // output : 1, 2, 3, 4, 5,
+  }
+}
